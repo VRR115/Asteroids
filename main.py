2b4304f943da8b61232f8f1d9f82959a84cb7e4e
VRR115 <kvudaykr@gmail.com>
2025-06-20 15:40:35 +0530

    Fix Player, adjust main.py for working game loop

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import pygame 
 from constants import *
+from player import Player
 
 
-
-
-def main():
+def main():  
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    while 1 :
+    updatable  = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    Player.containers = (updatable , drawable )
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt=0
+
+    while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        updatable.update(dt)
+        screen.fill((0,0,0))  
+
+        for o in drawable:
+            o.draw(screen)
+
         pygame.display.flip() 
+        dt =clock.tick(60) / 1000
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
